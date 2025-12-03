8a6aaf50a90a0e761ac1ab5f9b84ae48fd7831ca
faik dursun <250541124@firat.edu.tr>
2025-12-03 17:36:16 +0300

    Exercise4_2.java
    Added comments to clarify execution order.

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,29 @@
-public class Exercise4_2 {
+faik dursun
+  250541124
+  Exercise 4.2 tamamlandı.
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
+
+
+
+
+public class EXERCISE42 {
+
 
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        zippo("rattle",13 );   //1
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println( blimp);          // 5
+        zippo ("ping", -5 );   // 4,6
     }
-
     public static void zippo(String quince, int flag) {
         if (flag < 0) {
-            System.out.println(quince + " zoop");
-        } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println(quince + "zoop"); // 7
+        }else  {
+             System.out.println("ik");         // 2
+             baffle(quince);                  //3
+             System.out.println("boo-wa-ha-ha"); // 8
         }
     }
 }
